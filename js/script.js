7032685e4004b6d3f9c33b68c3387b617a9840de
lucas <lucas.cordeiro@totvs.com.br>
2026-07-30 22:23:55 -0300

    Funções para media

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ APROVADO: média >= 6
 REPROVADO: média < 6
 
 */ 
-const nomeAluno = prompt("Digite o nome do aluno:");
+/*const nomeAluno = prompt("Digite o nome do aluno:");
 const nota1 = parseFloat(prompt("Digite a nota 1:"));
 const nota2 = parseFloat(prompt("Digite a nota 2:"));   
 const nota3 = parseFloat(prompt("Digite a nota 3:")); 
@@ -28,4 +28,34 @@ if(!verBoletim){
     const boletim = `${nomeAluno} - ${status}\nNota 1: ${nota1}\nNota 2: ${nota2}\nNota 3: ${nota3}\nMédia: ${media.toFixed(2)}`;
     alert(boletim);
     console.log(boletim);
+}*/
+
+/** FUNÇÕES */
+
+function calcularMedia(nota1, nota2, nota3) {
+    return (nota1 + nota2 + nota3) / 3;
+}
+
+function obterStatus(media) {
+    return media >= 6 ? "APROVADO" : "REPROVADO";
+}
+
+function exibirBoletim(nomeAluno, nota1, nota2, nota3, media, status) {
+    const boletim = `${nomeAluno} - ${status}\nNota 1: ${nota1}\nNota 2: ${nota2}\nNota 3: ${nota3}\nMédia: ${media.toFixed(2)}`;
+    alert(boletim);
+}
+
+const nomeAluno = prompt("Digite o nome do aluno:");
+const nota1 = parseFloat(prompt("Digite a nota 1:"));
+const nota2 = parseFloat(prompt("Digite a nota 2:"));   
+const nota3 = parseFloat(prompt("Digite a nota 3:")); 
+const media = calcularMedia(nota1, nota2, nota3);
+const status = obterStatus(media);
+const verBoletim = confirm("Deseja visualizar o boletim completo?");
+
+if(!verBoletim){
+    alert("Operação cancelada");
+    console.log("Operação cancelada");
+}else{
+    exibirBoletim(nomeAluno, nota1, nota2, nota3, media, status);
 }
