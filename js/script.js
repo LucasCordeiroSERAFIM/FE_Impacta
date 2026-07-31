e89e0a78439c6fb08ca660877fc5b8bb3a494283
lucas <lucas.cordeiro@totvs.com.br>
2026-07-30 21:58:44 -0300

    Atividade Javascript Média

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ Digite a nota 3
 
 APROVADO: média >= 6
 REPROVADO: média < 6
+
 */ 
 const nomeAluno = prompt("Digite o nome do aluno:");
 const nota1 = parseFloat(prompt("Digite a nota 1:"));
@@ -18,8 +19,19 @@ const nota3 = parseFloat(prompt("Digite a nota 3:"));
 let soma = (nota1+nota1+nota3);
 let media = soma/3;
 
-if(media >= 6){
-    alert(`O aluno ${nomeAluno} foi APROVADO com média ${media.toFixed(2)}`);
-}else{
-    alert(`O aluno ${nomeAluno} foi REPROVADO com média ${media.toFixed(2)}`);
+const verBoletim = confirm("Deseja visualizar o boletim completo?");
+if(verBoletim === true && media >= 6){
+ alert(`O aluno ${nomeAluno} foi APROVADO com \nNota 1: ${nota1}\nNota 2: ${nota2}\nNota 3: ${nota3}\nMédia: ${media.toFixed(2)}`);
+    console.log(`O aluno ${nomeAluno} foi APROVADO com \nNota 1: ${nota1}\nNota 2: ${nota2}\nNota 3: ${nota3}\nMédia: ${media.toFixed(2)}`);
+}else if(verBoletim === true && media < 6){
+     alert(`O aluno ${nomeAluno} foi REPROVADO com \nNota 1: ${nota1}\nNota 2: ${nota2}\nNota 3: ${nota3}\nMédia: ${media.toFixed(2)}`);
+    console.log(`O aluno ${nomeAluno} foi REPROVADO com \nNota 1: ${nota1}\nNota 2: ${nota2}\nNota 3: ${nota3}\nMédia: ${media.toFixed(2)}`);   
+    
+}else if(verBoletim === false && media >= 6){
+    alert(`O aluno ${nomeAluno} foi APROVADO com média: ${media.toFixed(2)}`);
+    console.log(`O aluno ${nomeAluno} foi APROVADO com média: ${media.toFixed(2)}`);
+}else if(verBoletim === false && media < 6){
+    alert(`O aluno ${nomeAluno} foi REPROVADO com média: ${media.toFixed(2)}`);
+    console.log(`O aluno ${nomeAluno} foi REPROVADO com média: ${media.toFixed(2)}`);
 }   
+
